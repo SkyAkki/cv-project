69b5cdc58bbcca07d86dfd28240d1c166e72f96a
Akash Saxena <akashsaxena5555@gmail.com>
2022-11-04 19:40:36 +0530

    move Button Components out, inside respective sections

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import Section from './components/Section'
 import './styles/style.css'
-import ConfirmButton from './components/ConfirmButton'
-import EditButton from './components/EditButton'
+import Education from './components/Education'
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      isBasicOn : false,
-      isBasicEdit : true
-    }
-    this.setBasicAndEdit = this.setBasicAndEdit.bind(this)
-  }
-  setBasicAndEdit(basicValue,editValue){
-    this.setState({
-      isBasicOn : basicValue,
-      isBasicEdit : editValue
-    })
-  }
   render(){
     return (
       <div>
         <Section></Section>
-        <ConfirmButton setBasicAndEdit = {this.setBasicAndEdit} editProp = {this.state.isBasicEdit}></ConfirmButton>
-        <EditButton setBasicAndEdit = {this.setBasicAndEdit}/>
+        <Education></Education>
       </div>
     );
   }
